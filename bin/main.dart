3f2e99b40b947b6178c77f9caa6aa86d4417a4c5
Nafisa Chiquita Finandra Putri <nafisachiquita06@gmail.com>
2026-03-09 00:09:56 +0700

    Add scoring feature for players

--- bin/main.dart
+++ bin/main.dart
@@ -6,17 +6,31 @@ void main() {
   int randomNum = random.nextInt(100) + 1;
   int guess = 0;
 
+  int maxAttempts = 9;
+  int attempts = 0;
+
   do {
-    stdout.write("Tebak angka antara 1 dan 100 : ");
+    stdout.write("Tebak angka antara 1 dan 100 (sisa ${maxAttempts - attempts} kesempatan) : ");
     String? input = stdin.readLineSync();
-    guess = int.parse(input ?? "0");
+    guess = int.tryParse(input ?? "") ?? 0;
+
+    attempts++;
 
     if (guess > randomNum) {
       print("Terlalu besar!\n");
     } else if (guess < randomNum) {
       print("Terlalu kecil!\n");
     } else {
-      print("Tebakan benar!");
+      int score = (maxAttempts - attempts + 1) * 10;
+      print("Tebakan benar! Kamu menebak dalam $attempts percobaan!");
+      print("Score kamu : $score");
+      break;
     }
+
+    if (attempts >= maxAttempts && guess != randomNum) {
+      print("Maaf kesempatan habis! Angka yang benar adalah $randomNum");
+      break;
+    }
+
   } while (guess != randomNum);
 }
