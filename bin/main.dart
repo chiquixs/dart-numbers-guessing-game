b32b308d84a2625c0d1c042ad920c825c7f13335
Nafisa Chiquita Finandra Putri <nafisachiquita06@gmail.com>
2026-03-09 00:17:18 +0700

    adding a “play again or not” feature

--- bin/main.dart
+++ bin/main.dart
@@ -2,21 +2,45 @@ import 'dart:math';
 import 'dart:io';
 
 void main() {
-  Random random = Random();
-  int randomNum = random.nextInt(100) + 1;
-  int guess = 0;
+  bool playAgain = true;
 
-  do {
-    stdout.write("Tebak angka antara 1 dan 100 : ");
-    String? input = stdin.readLineSync();
-    guess = int.parse(input ?? "0");
+  while (playAgain) {
+    Random random = Random();
+    int randomNum = random.nextInt(100) + 1;
+    int guess = 0;
 
-    if (guess > randomNum) {
-      print("Terlalu besar!\n");
-    } else if (guess < randomNum) {
-      print("Terlalu kecil!\n");
-    } else {
-      print("Tebakan benar!");
-    }
-  } while (guess != randomNum);
+    int maxAttempts = 9;
+    int attempts = 0;
+
+    do {
+      stdout.write("Tebak angka antara 1 dan 100 (sisa ${maxAttempts - attempts} kesempatan) : ");
+      String? input = stdin.readLineSync();
+      guess = int.tryParse(input ?? "") ?? 0;
+
+      attempts++;
+
+      if (guess > randomNum) {
+        print("Terlalu besar!\n");
+      } else if (guess < randomNum) {
+        print("Terlalu kecil!\n");
+      } else {
+        int score = (maxAttempts - attempts + 1) * 10;
+        print("Tebakan benar! Kamu menebak dalam $attempts percobaan!");
+        print("Score kamu : $score");
+        break;
+      }
+
+      if (attempts >= maxAttempts && guess != randomNum) {
+        print("Maaf kesempatan habis! Angka yang benar adalah $randomNum");
+        break;
+      }
+
+    } while (guess != randomNum);
+
+    stdout.write("\nMaw main lagi? (y/n) : ");
+    String? again = stdin.readLineSync();
+    playAgain = (again != null && again.toLowerCase() == 'y');
+  }
+
+  print("\nTerima kasih sudah bermain!");
 }
